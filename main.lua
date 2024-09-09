8d8ab729759665e0f0167eb547223d61319d7186
Phluso <pedrolusocosta@gmail.com>
2024-09-08 21:38:40 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 require "Lua Library"
 
 function love.load()
-    love.window.setFullscreen(false)
+    love.window.setFullscreen(true)
 
     window = {}
     window.lar = love.graphics.getWidth()
@@ -13,15 +13,22 @@ function love.load()
 
     id = 0
 
-    local radius = 5000
-    for i = 0, 1000, 1 do
+    --nBola(4096, 0, 0, 0, 0)
+
+    --[[local radius = 50000
+    for i = 0, 500, 1 do
         local x = love.math.random() * love.graphics.getWidth()
         local y = love.math.random() * love.graphics.getHeight()
         local dist = distance(x, y, window.centerx, window.centery)
         local xspd = lenx(window.centerx, x, dist)
         local yspd = leny(window.centery, y, dist)
-        nBola(love.math.random() * 10, xspd * love.math.random() * radius, 0 + yspd * love.math.random() * radius, 0, 0)
-    end
+        nBola(80, xspd * love.math.random() * radius, 0 + yspd * love.math.random() * radius, 0, 0)
+    end]]
+    nBola(80, 0, 0, 0, 0, false)
+    nBola(1, 0, 500, 3.61, 0)
+    nBola(2, 0, 1500, 4.2, 0)
+    nBola(15, 0, 10000, 11.85, 0)
+    nBola(20, 0, 20000, 11.85, 0)
 
     
     cam = {}
@@ -29,7 +36,7 @@ function love.load()
     cam.y = 0
     cam.width = window.lar / 3
     cam.height = window.alt / 3
-    cam.zoom = 1
+    cam.zoom = .025
 
     cursor = {}
     cursor.x = 0
@@ -41,7 +48,7 @@ function love.load()
     --initial simulation speed
     simSpd = 1
 
-    mass = 10
+    mass = 80
 
     placingObject = false
     mira = {}
@@ -62,12 +69,14 @@ function love.load()
 end
 
 function love.update(dt)
+
     --coordenadas do mouse em relação ao mundo, não a tela
     mousex = ((love.mouse.getX()) / (window.lar) - .5) * cam.width + cam.x
     mousey = ((love.mouse.getY()) / (window.alt) - .5) * cam.height + cam.y
 
     local camSpd = 200
     
+    --velocidade da simulação
     if (pause == false) then
         simSpd = 1
     else
@@ -139,6 +148,7 @@ function love.update(dt)
     if (love.keyboard.isDown("e")) then
         cam.zoom = cam.zoom + (cam.zoom / 2.5) * dt
     end
+    cam.zoom = clamp(cam.zoom, 0, 100)
     cam.width = window.lar / cam.zoom
     cam.height = window.alt / cam.zoom
 
@@ -146,16 +156,39 @@ function love.update(dt)
     --|INTERACTIONS|
     --+------------+
     local maxInt = #bolas * precision + 1
-    if (simSpd > 0) then                            --caso não esteja pausado
-        for t = 0, simSpd, 1 do                     --repetir os cálculos de acordo com a velocidade da simulação
-            for i, v in ipairs(bolas) do            --calcular para cada objeto
-                if (love.mouse.isDown(3)) then
-                    if (circleColision(mousex, mousey, v.x, v.y, v.volume * cam.zoom)) then
-                        seguindo.id = v
-                    end
+    for t = 0, simSpd, 1 do                     --repetir os cálculos de acordo com a velocidade da simulação
+        for i, v in ipairs(bolas) do            --calcular para cada objeto
+            --seguir
+            if (love.mouse.isDown(3)) then
+                if (circleColision(mousex, mousey, v.x, v.y, (v.massa / v.densidade * 3.14) * cam.zoom)) then
+                    seguindo.id = v
                 end
-                --remover se estiver muito longe do ponto inicial
-                if not(collision(v.x, v.y, -50000, -50000, 50000, 50000)) then table.remove(bolas, i) end
+            end
+            if (simSpd > 0) then                --calcular interações somente caso não esteja pausado
+            --adcionar posição nas arrays de rastro
+            v.tailTime = v.tailTime - dt
+            if (v.tailTime <= 0) then
+                if (#v.tailX < 300) then
+                    --adicionar posição atual
+                    table.insert(v.tailX, v.x)
+                    table.insert(v.tailY, v.y)
+                else
+                    --remover primeiro indice
+                    table.remove(v.tailX, 1)
+                    table.remove(v.tailY, 1)
+                    --adicionar posição atual
+                    table.insert(v.tailX, v.x)
+                    table.insert(v.tailY, v.y)
+                end
+                v.tailTime = 10
+            end
+            --alterar a densidade no caso de estrelas
+            if (v.tipo == "estrela madura" or v.tipo == "gigante vermelha" or v.tipo == "gigante azul") then
+                v.densidade = v.densidade - .00001 * dt
+            end
+            bolaSpecs(v)
+            --remover se estiver muito longe do ponto inicial
+                if not(collision(v.x, v.y, -500000, -500000, 500000, 500000)) then table.remove(bolas, i) end
                 for j, u in ipairs(bolas) do        --calcular as interações
                     if (j < maxInt) then            --limitar interações
                         if (v.fixed == nil or v.fixed == false) then
@@ -164,11 +197,12 @@ function love.update(dt)
                                 v.xspd = v.xspd + lenx(v.x, u.x, distance(v.x, v.y, u.x, u.y)) * atracao
                                 v.yspd = v.yspd + leny(v.y, u.y, distance(v.x, v.y, u.x, u.y)) * atracao
                                 --colisão entre os objetos
-                                if (circleColision(v.x, v.y, u.x + (v.volume * sign(v.x - u.x)), u.y + (v.volume * sign(v.y - u.y)), u.volume)) then
+                                local vvolume = (v.massa / v.densidade * 3.14)
+                                local uvolume = (u.massa / u.densidade * 3.14)
+                                if (circleColision(v.x + v.xspd, v.y + v.yspd, u.x + u.xspd + (vvolume * sign(v.x - u.x)), u.y + u.yspd + (vvolume * sign(v.y - u.y)), uvolume)) then
                                     if (u.massa >= v.massa) then
-                                        u.massa = u.massa + v.massa / 10
+                                        u.massa = u.massa + v.massa
                                         table.remove(bolas, i)
-                                        bolaSpecs(u) 
                                     end
                                 end
                             end
@@ -184,8 +218,8 @@ function love.update(dt)
 
     --resetar
     if (love.keyboard.isDown("tab")) then
-        for i, v in ipairs(bolas) do
-            table.remove(bolas, i)
+        for i = 0, #bolas, 1 do
+            bolas[i] = nil
         end
     end 
 
@@ -195,7 +229,7 @@ function love.update(dt)
         local randx = (love.math.random() - .5) * 50 / cam.zoom
         local randy = (love.math.random() - .5) * 50 / cam.zoom
 
-        nBola(.1, mousex + randx, mousey + randy, 0, 0)
+        nBola(.02, mousex + randx, mousey + randy, 0, 0)
     end 
     if (love.mouse.isDown(2)) then
         if (placingObject == false) then
@@ -229,8 +263,8 @@ function love.update(dt)
     end
 
     --limitar posição da câmera
-    cam.x = clamp(cam.x, -50000, 50000)
-    cam.y = clamp(cam.y, -50000, 50000)
+    cam.x = clamp(cam.x, -500000, 500000)
+    cam.y = clamp(cam.y, -500000, 500000)
 
     --somar tempo
     time = time + dt * 100 * simSpd
@@ -260,11 +294,21 @@ function love.draw()
         local x = (v.x - minx) / (maxx - minx) * window.lar
         local y = (v.y - miny) / (maxy - miny) * window.alt
 
+        love.graphics.setColor(1, 1, 1, .3)
+
+        for t = 2, #v.tailX, 1 do
+            local tx1 = (v.tailX[t] - minx) / (maxx - minx) * window.lar
+            local ty1 = (v.tailY[t] - miny) / (maxy - miny) * window.alt
+
+            local tx2 = (v.tailX[t-1] - minx) / (maxx - minx) * window.lar
+            local ty2 = (v.tailY[t-1] - miny) / (maxy - miny) * window.alt
+            --desenhar rastro
+            love.graphics.line(tx1, ty1, tx2, ty2)
+        end
+
+        --desenhar objeto
         love.graphics.setColor(v.cor.r, v.cor.g, v.cor.b, 1)
-        love.graphics.circle("fill", x, y, v.volume * cam.zoom) 
-        love.graphics.setColor(1, 1, 1, 1)
-        love.graphics.line(x, y, x + v.xspd, y + v.yspd)
-        --love.graphics.print(distance(x, y, x + v.xspd, y + v.yspd), x, y - 10)
+        love.graphics.circle("fill", x, y, clamp((v.massa / v.densidade * 3.14) * cam.zoom, 1, 999999)) 
     end
     
     --desenhar mira
@@ -286,6 +330,8 @@ function love.draw()
     if (seguindo.id ~= nil) then
         love.graphics.print("Following type: " .. seguindo.id.tipo, 10, 170)
         love.graphics.print("Following mass: " .. seguindo.id.massa, 10, 190)
+        love.graphics.print("Following density: " .. seguindo.id.densidade, 10, 210)
+        love.graphics.print("Following volume: " .. tostring(seguindo.id.massa / seguindo.id.densidade * 3.14), 10, 230)
     end
 end
 
@@ -293,8 +339,7 @@ function nBola(massa, x, y, xspd, yspd, fixed)
     local bola = {}
     bola.id = id
     bola.massa = massa
-    bola.densidade = nil
-    bola.volume = nil
+    bola.densidade = 1
     bola.tipo   = nil
     bola.cor = {}
     bola.cor.r = .7
@@ -306,6 +351,10 @@ function nBola(massa, x, y, xspd, yspd, fixed)
     bola.yspd = yspd
     bola.fixed = fixed
 
+    bola.tailX = {x}
+    bola.tailY = {y}
+    bola.tailTime = 1
+
     id = id + 1
 
     bolaSpecs(bola)
@@ -314,65 +363,75 @@ function nBola(massa, x, y, xspd, yspd, fixed)
 end
 
 function bolaSpecs(index)
+    local terraMassa = 1
+    local solMassa = 80
+
     --calcular massa e tipo do corpo celeste
-    if (index.massa >= 1024) then
-        index.tipo = "buraco negro"
-        index.cor.r = .1
-        index.cor.g = .1
-        index.cor.b = .1
-        index.densidade = 255
-    elseif (index.massa >= 700) then
-        index.tipo = "anã branca"
+    if (index.densidade >= 255) or (index.tipo == "buraco negro") then index.tipo = "buraco negro"
+    elseif (index.densidade <= .15) or (index.tipo == "anã branca") then index.tipo = "anã branca"
+    elseif (index.massa >= solMassa * 10) or (index.densidade <= .25) then index.tipo = "gigante azul"
+    elseif (index.massa >= solMassa * 7) or (index.densidade <= .5) then index.tipo = "gigante vermelha"
+    elseif (index.massa >= solMassa) then index.tipo = "estrela madura"
+    elseif (index.massa >= terraMassa * 40) then index.tipo = "anã marrom"
+    elseif (index.massa >= terraMassa * 15) then index.tipo = "gigante" 
+    elseif (index.massa >= terraMassa) then index.tipo = "planeta"
+    else index.tipo = "asteroide" end
+
+    if (index.densidade <= .01) then index.tipo = "nebulosa" end
+
+    if (index.tipo == "anã branca") then  --estágio final
         index.cor.r = 1
         index.cor.g = 1
         index.cor.b = 1
-        index.densidade = 10
-    elseif (index.massa >= 600) then
-        index.tipo = "gigante azul"
-        index.cor.r = .15
-        index.cor.g = .15
+        index.densidade = 80
+        if (index.massa >= solMassa * 12) then
+            index.tipo = "buraco negro"
+            index.cor.r = .3
+            index.cor.g = .3
+            index.cor.b = .3
+            index.densidade = 512
+        end
+    elseif (index.tipo == "gigante azul") then
+        index.cor.r = .6
+        index.cor.g = .6
         index.cor.b = 1
-        index.densidade = .35
-    elseif (index.massa >= 500) then
-        index.tipo = "gigante vermelha"
+        --index.densidade = .45
+    elseif (index.tipo == "gigante vermelha") then     --início do estágio de gigante vermelha
         index.cor.r = 1
         index.cor.g = .25
         index.cor.b = 0
-        index.densidade = .4
-    elseif (index.massa >= 300) then
-        index.tipo = "estrela"
+        --index.densidade = .6
+    elseif (index.tipo == "estrela madura") then         --fase madura da estrela
         index.cor.r = 1
         index.cor.g = 1
         index.cor.b = 0
-        index.densidade = .6
-    elseif (index.massa >= 110) then
-        index.tipo = "anã marrom"
+        --index.densidade = .8
+    elseif (index.tipo == "anã marrom") then         --anã marrom ou protoestrela
         index.cor.r = .5
         index.cor.g = .25
         index.cor.b = .25
-        index.densidade = .75
-    elseif (index.massa >= 30) then
-        index.tipo = "planeta gasoso"
+        index.densidade = .95
+    elseif (index.tipo == "gigante") then
         index.cor.r = .75
         index.cor.g = .5
         index.cor.b = .25
-        index.densidade = .5
-    elseif (index.massa >= 1) then
-        index.tipo = "planeta rochoso"
+        index.densidade = .95
+    elseif (index.tipo == "planeta") then
         index.cor.r = .2
         index.cor.g = .5
         index.cor.b = .7
         index.densidade = 1
-    elseif (index.massa < 1) then
-        index.tipo = "asteroide"
+    elseif (index.tipo == "asteroide") then
         index.cor.r = .5
         index.cor.g = .1
         index.cor.b = .2
-        index.densidade = .75
+        index.densidade = 1.5
+    elseif (index.tipo == "nebulosa") then
+        index.cor.r = .5
+        index.cor.g = .5
+        index.cor.b = .5
+        index.densidade = .0001
     end
-
-    --calcular o volume do planeta com base na massa e densidade
-    index.volume = index.massa / index.densidade * 3.14
 end
 
 function love.keypressed(key)
